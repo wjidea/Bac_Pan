d885f249af18ad19adc04a00635b67da9b2cc381
Jie Wang <wjidea@gmail.com>
2015-09-17 11:32:32 -0400

    include more codes

--- Core_genome_estimation.R
+++ Core_genome_estimation.R
@@ -49,16 +49,24 @@ core_genes <- function(s1 = 3,s2 = 4,s3 = 5){
 # 4. group2 turf (SH7, QH1, MOR, COLB1, INDB2, SF12)
 # 5. all turf and maize pathogens (15 strains)
 # 6. all strains 15 strains + Cat98-1
-colnames(work_data)[3:18]
-turf_strains_index <- c(4,6:18)
-maize_strains_index <- c(1:3)
+
+#colnames(work_data)[3:18]
+turf_strains_index <- c(6,8:18)
+colnames(work_data)[turf_strains_index]
+maize_strains_index <- c(3:5)
+colnames(work_data)[maize_strains_index]
 turf_g1_index <- c(9,10,11,13,15)
 colnames(work_data)[turf_g1_index]
 turf_g2_index <- c(17,14,12,6,8,16)
 colnames(work_data)[turf_g2_index]
 turf_maize_index <- c(3:6,8:18)
+colnames(work_data)[turf_maize_index]
 all_index <- c(3:18)
-
+colnames(work_data)[all_index]
+g1_maize_index <- c(9,10,11,13,15,3:5)
+colnames(work_data)[g1_maize_index]
+g2_maize_index <- c(17,14,12,6,8,16,3:5)
+colnames(work_data)[g2_maize_index]
 # task 1 maize pathogens (3 strains) AA78, Aa99, AA38
 T1 <- core_genes()
 T1
@@ -75,6 +83,7 @@ boxplot(store_coreset_2, main = "12 Turf strain")
 
 # task 3 group1 turf (KL3, INV, QHB1 MDB1, NCT3)
 store_coreset_3 <- c()
+i <- 0
 for (i in 1:1000){
   sub_strains <- turf_g1_index[sample(1:length(turf_g1_index),3,replace = FALSE)]
   store_coreset_3[i] <- core_genes(sub_strains[1],sub_strains[2],sub_strains[3])
@@ -84,6 +93,7 @@ boxplot(store_coreset_3, main = "group 1 Turf strain")
 
 # Task 4 group2 turf (SH7, QH1, MOR, COLB1, INDB2, SF12)
 store_coreset_4 <- c()
+i <- 0
 for (i in 1:1000){
   sub_strains <- turf_g2_index[sample(1:length(turf_g2_index),3,replace = FALSE)]
   store_coreset_4[i] <- core_genes(sub_strains[1],sub_strains[2],sub_strains[3])
@@ -93,6 +103,7 @@ boxplot(store_coreset_4, main = "group 2 Turf strain")
 
 # task 5 all turf and maize pathogens (15 strains)
 store_coreset_5 <- c()
+i <- 0
 for (i in 1:1000){
   sub_strains <- turf_maize_index[sample(1:length(turf_maize_index),3,replace = FALSE)]
   store_coreset_5[i] <- core_genes(sub_strains[1],sub_strains[2],sub_strains[3])
@@ -102,6 +113,7 @@ boxplot(store_coreset_5, main = "turf and maize strains")
 
 # task 6 all strains 15 strains + Cat98-1
 store_coreset_6 <- c()
+i <- 0
 for (i in 1:1000){
   sub_strains <- all_index[sample(1:length(all_index),3,replace = FALSE)]
   store_coreset_6[i] <- core_genes(sub_strains[1],sub_strains[2],sub_strains[3])
@@ -109,8 +121,34 @@ for (i in 1:1000){
 summary(store_coreset_6)
 boxplot(store_coreset_6, main = "all strains")
 
+# task 7 group1 and maize strains
+store_coreset_7 <- c()
+i <- 0
+for (i in 1:1000){
+  sub_strains <- g1_maize_index[sample(1:length(g1_maize_index),3,replace = FALSE)]
+  store_coreset_7[i] <- core_genes(sub_strains[1],sub_strains[2],sub_strains[3])
+}
+summary(store_coreset_7)
+boxplot(store_coreset_7, main = "group1 and maize strains")
+# task 8 group2 and maize strains
+store_coreset_8 <- c()
+i <- 0
+for (i in 1:1000){
+  sub_strains <- g2_maize_index[sample(1:length(g2_maize_index),3,replace = FALSE)]
+  store_coreset_8[i] <- core_genes(sub_strains[1],sub_strains[2],sub_strains[3])
+}
+summary(store_coreset_8)
+boxplot(store_coreset_8, main = "group1 and maize strains")
+
 # store all data into a dataframe
-all_strains <- cbind(store_coreset_2,store_coreset_3,store_coreset_4,
-                     store_coreset_5,store_coreset_6)
-boxplot(all_strains)
+all_strains <- cbind(store_coreset_1 = c(2330),store_coreset_3,store_coreset_4,
+                     store_coreset_2, store_coreset_5,store_coreset_7,
+                     store_coreset_8,store_coreset_6)
+all_strains <- rename(as.data.frame(all_strains),maize_strains = store_coreset_1, 
+                      turf_strains = store_coreset_2, 
+                      group1_turf = store_coreset_3,group2_turf = store_coreset_4,
+                      maize_turf = store_coreset_5,all_strains = store_coreset_6,
+                      g1_maize = store_coreset_7, g2_maize = store_coreset_8)
+boxplot(all_strains, main = "Core genome size resampling", ylab = "Number of genes",
+        xlab = "group of strains")
 apply(all_strains,2,summary)
